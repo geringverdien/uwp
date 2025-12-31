d32e5e376d79cf8c41542572dd6ee79d7d698ece
binury <1146921+binury@users.noreply.github.com>
2025-12-30 20:54:55 -0500

    Add: Discord menu button

--- gd/mods/uwp/main.gd
+++ gd/mods/uwp/main.gd
@@ -1,25 +1,44 @@
 extends Node
 
-## TODO!
+var menu_button = preload("res://mods/uwp/Scenes/discord_button.tscn")
+
 var patches := [
 	preload("res://mods/uwp/patches/persistent-ignores.gd"),
 	preload("res://mods/uwp/patches/unlimit-props.gd")
 ]
 
 
-func _ready():
+func _ready() -> void:
 	get_tree().connect("node_added", self, "_join_tree")
+	get_tree().connect("node_added", self, "_add_menu_button")
 
 
-func _load_patches():
+func _load_patches() -> void:
 	for patch in patches:
 		add_child(patch.new())
 
 
-func _join_tree(node):
+func _join_tree(node: Node) -> void:
 	var map: Node = get_tree().current_scene
 	var in_game = map.name == "world"
 	if in_game:
 		get_tree().disconnect("node_added", self, "_join_tree")
 		self._load_patches()
+	
 
+
+func _add_menu_button(menu: Node) -> void:
+	if menu.name != "main_menu": return
+	var menu_buttons: Node = menu.get_node("VBoxContainer")
+
+	var button: Button = menu_button.instance()
+	button.connect("pressed", self, "_on_button_pressed")
+
+	var settings_button: Node = menu_buttons.get_node("settings")
+	menu_buttons.add_child(button)
+	menu_buttons.move_child(button, settings_button.get_index())
+	menu_buttons.margin_top -= 48
+
+
+func _on_button_pressed() -> void:
+	OS.shell_open("https://discord.gg/kjf3FCAMDb")
